2fa53ce69e4bbdef49e879945d7ad940463ebb20
spencer119 <spencerhamilton119@gmail.com>
2020-05-21 20:50:28 -0600

    fsd

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.static('./data/'));
 app.use(express.json());
 app.use(fileUpload());
-const maintenance = true;
+const maintenance = false;
 
 // Routes
 app.use((req, res, next) => {
