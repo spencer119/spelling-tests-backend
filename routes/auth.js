e70f228e306132a4c2804e7dc1675001266d7a03
Spencer Hamilton <hamiltons@mdsd.org>
2020-10-07 10:17:25 -0600

    change password on first login

--- routes/auth.js
+++ routes/auth.js
@@ -15,13 +15,43 @@ router.post('/teacher', (req, res) => {
         jwt.sign(data.rows[0], process.env.JWT_SECRET, (err, token) => {
           if (err) {
             return res.status(500).json({ msg: 'Authentication error' });
+          }
+          else if (password === 'eagles2020') {
+            res.json({firstLogin: true, token})
           } else {
             res.json({ token });
           }
         });
+      } else {
+        return res.status(401).json({msg: 'Invalid credentials.'})
       }
     }
   );
+});
+router.post('/teacher/change/password', (req, res) => {
+  let token = req.headers.token;
+  jwt.verify(token, process.env.JWT_SECRET, async (err, auth) => {
+    if (err) {
+      return res.status(403);
+    }
+    if (auth.teacher_id) {
+      let newHash = bcrypt.hashSync(req.body.password);
+      console.log(`${req.body.password} : ${newHash}`)
+      db.query(
+        `UPDATE teachers SET password = '${newHash}' WHERE teacher_id = '${auth.teacher_id}'`,
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ err });
+          } else {
+            res.status(200).json(data);
+          }
+        }
+      );
+    } else {
+      return res.status(403).json({ msg: 'Unauthorized' });
+    }
+  });
 });
 router.post('/student', (req, res) => {
   let username = req.body.username;
