12b6c593504e6187024ea19c2733c8fcabd9bceb
spencer119 <spencerhamilton119@gmail.com>
2020-11-01 18:24:35 -0700

    can reset teacher passwords

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv/config');
 const fileUpload = require('express-fileupload');
 const app = express();
@@ -12,12 +13,40 @@ app.use(fileUpload());
 const maintenance = false;
 app.options('*', cors())
 
+const checkAuth = async (req,res,next) => {
+  let token = req.headers.token
+  if(req.path.startsWith('/api/teacher')) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, auth) => {
+      if(err) {
+        console.error(err)
+        return res.status(403)
+      } else if(auth.teacher_id) {
+        res.locals.auth = auth
+        next();
+      } else return res.status(403)
+    })
+  } else if(req.path.startsWith('/api/admin')) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, auth) => {
+      if(err) {
+        console.error(err)
+        return res.status(403)
+      } else if(auth.is_admin) {
+        res.locals.auth = auth
+        next();
+      } else return res.status(403)
+    })
+  } else next();
+}
+
+
 // Routes
+app.use(checkAuth)
 app.use('/api/teacher', require('./routes/teacher'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/results', require('./routes/results'));
 app.use('/api/student', require('./routes/student'));
 app.use('/api/admin', require('./routes/admin'));
+
 
 // Connect to database
 mongoose.connect(
