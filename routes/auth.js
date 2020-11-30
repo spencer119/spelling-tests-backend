0086700e4ec193e501148d10f06301a9b2955926
Spencer Hamilton <hamiltons@mdsd.org>
2020-11-30 10:21:31 -0700

    scores and token fixes

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.post('/teacher', (req, res) => {
       if (data.rows.length === 0 || err) {
         return res.status(401).json({ msg: 'Invalid credentials.' });
       } else if (bcrypt.compareSync(password, data.rows[0].password)) {
-        jwt.sign(data.rows[0], process.env.JWT_SECRET,{expiresIn: '20s'}, (err, token) => {
+        jwt.sign(data.rows[0],  process.env.JWT_SECRET,{expiresIn: '2h'}, (err, token) => {
           if (err) {
             console.log(err)
             return res.status(500).json({ msg: 'Authentication error' });
@@ -73,7 +73,7 @@ router.post('/student', (req, res) => {
       if (data.rows.length === 0 || err) {
         return res.status(401).json({ msg: 'Invalid username' });
       } else {
-        jwt.sign(data.rows[0], {expiresIn: '2h'}, process.env.JWT_SECRET, (err, token) => {
+        jwt.sign(data.rows[0], process.env.JWT_SECRET,{expiresIn: '2h'}, (err, token) => {
           if (err) {
             return res.status(500).json({ msg: 'Authentication error' });
           } else {
