ae7fce3a9567a4360cf714f7b81a39bfa52f6977
spencer119 <spencernhamilton@gmail.com>
2021-02-10 19:33:27 -0700

    v2.2.1 apostrophe issue fixed

--- routes/student.js
+++ routes/student.js
@@ -4,28 +4,48 @@ const jwt = require('jsonwebtoken');
 require('dotenv/config');
 const db = require('../db');
 router.get('/testId', async (req, res) => {
-  let auth = res.locals.auth
-  let activeTest = await db.query(`SELECT active_test FROM groups WHERE group_id='${auth.group_id}'`)
-  if (activeTest.rows[0].active_test === null) return res.status(200).json({test_id: null, first_name: auth.first_name})
+  let auth = res.locals.auth;
+  let activeTest = await db.query(
+    `SELECT active_test FROM groups WHERE group_id='${auth.group_id}'`
+  );
+  if (activeTest.rows[0].active_test === null)
+    return res.status(200).json({ test_id: null, first_name: auth.first_name });
   else {
-    let allowedAttempts = await db.query(`SELECT attempts FROM tests WHERE test_id = '${activeTest.rows[0].active_test}'`)
-    let attempts = await db.query(`SELECT * FROM results WHERE test_id = '${activeTest.rows[0].active_test}' AND student_id = '${auth.student_id}'`)
+    let allowedAttempts = await db.query(
+      `SELECT attempts FROM tests WHERE test_id = '${activeTest.rows[0].active_test}'`
+    );
+    let attempts = await db.query(
+      `SELECT * FROM results WHERE test_id = '${activeTest.rows[0].active_test}' AND student_id = '${auth.student_id}'`
+    );
     console.log(allowedAttempts.rows[0].attempts);
-    console.log(attempts.rows.length)
-    return res.status(200).json({test_id: activeTest.rows[0].active_test, first_name: auth.first_name, allowedAttempts: allowedAttempts.rows[0].attempts, attempts: attempts.rows.length})
+    console.log(attempts.rows.length);
+    return res.status(200).json({
+      test_id: activeTest.rows[0].active_test,
+      first_name: auth.first_name,
+      allowedAttempts: allowedAttempts.rows[0].attempts,
+      attempts: attempts.rows.length,
+    });
   }
 });
-router.get('/scores', async (req,res) => {
-  console.log('req')
+router.get('/scores', async (req, res) => {
+  console.log('req');
   let auth = res.locals.auth;
-  let groupID = await db.query(`SELECT group_id FROM students WHERE student_id = '${auth.student_id}'`)
-  let activeTest = await db.query(`SELECT active_test FROM groups WHERE group_id = '${groupID.rows[0].group_id}'`)
-  if (activeTest.rows[0].active_test === null) return res.status(200).json({results: null, resultData: null})
-  let results = await db.query(`SELECT * FROM results WHERE student_id='${auth.student_id}' AND test_id = '${activeTest.rows[0].active_test}'`)
-  let resultData = await db.query(`SELECT * FROM resultdata WHERE result_id IN (SELECT result_id FROM results WHERE test_id = '${activeTest.rows[0].active_test}' AND student_id = '${auth.student_id}')`)
-  return res.status(200).json({results: results.rows, resultData: resultData.rows})
-  
-})
+  let groupID = await db.query(
+    `SELECT group_id FROM students WHERE student_id = '${auth.student_id}'`
+  );
+  let activeTest = await db.query(
+    `SELECT active_test FROM groups WHERE group_id = '${groupID.rows[0].group_id}'`
+  );
+  if (activeTest.rows[0].active_test === null)
+    return res.status(200).json({ results: null, resultData: null });
+  let results = await db.query(
+    `SELECT * FROM results WHERE student_id='${auth.student_id}' AND test_id = '${activeTest.rows[0].active_test}'`
+  );
+  let resultData = await db.query(
+    `SELECT * FROM resultdata WHERE result_id IN (SELECT result_id FROM results WHERE test_id = '${activeTest.rows[0].active_test}' AND student_id = '${auth.student_id}')`
+  );
+  return res.status(200).json({ results: results.rows, resultData: resultData.rows });
+});
 router.get('/test', (req, res) => {
   db.query(
     `SELECT * FROM testlines WHERE test_id = '${req.query.test_id}' ORDER BY line_number`,
@@ -40,44 +60,58 @@ router.get('/test', (req, res) => {
 });
 router.post('/test/submit', (req, res) => {
   let token = req.headers.token;
-  let testData = req.body.testData
-  console.log(req.body)
+  let testData = req.body.testData;
+  console.log(req.body);
   jwt.verify(token, process.env.JWT_SECRET, async (err, auth) => {
     if (err) {
       return res.status(403);
     }
     let total = 0;
     let correct = 0;
-    testData.map(x => {
+    testData.map((x) => {
       if (x.ans.toLowerCase() === x.word.toLowerCase()) correct++;
       total++;
-    })
+    });
     let score = correct / total;
-    let attempts = await db.query(`SELECT attempt FROM results WHERE student_id = '${auth.student_id}' AND test_id = '${req.body.test_id}'`)
-    console.log(attempts)
-    db.query(`INSERT INTO results (student_id, test_id, teacher_id, group_id, correct, total, score, attempt) VALUES ('${auth.student_id}', '${req.body.test_id}', '${auth.teacher_id}', '${auth.group_id}', ${correct}, ${total}, ${score}, ${attempts.rowCount + 1}) RETURNING result_id`, (err, data) => {
-      if (err) {
-        console.error(err)
-        return res.status(500)
-      } else {
-        let queryString = `INSERT INTO resultdata (result_id, word, answer, correct, line_number) VALUES `;
-        let line_number = 1;
-        testData.forEach(x => {
-          queryString = queryString.concat(`('${data.rows[0].result_id}', '${x.word}', '${x.ans}', ${x.word === x.ans ? true : false}, ${line_number}),`)
-          line_number++;
-        })
-        queryString = queryString.slice(0, -1);
-        db.query(queryString, (err, sdata) => {
-          if (err) {
-            console.error(err);
-            db.query(`DELETE FROM results WHERE result_id = '${data.rows[0].result_id}'`)
-            return res.status(500)
-          } else {
-            res.status(200).json()
-          }
-        })
+    let attempts = await db.query(
+      `SELECT attempt FROM results WHERE student_id = '${auth.student_id}' AND test_id = '${req.body.test_id}'`
+    );
+    console.log(attempts);
+    db.query(
+      `INSERT INTO results (student_id, test_id, teacher_id, group_id, correct, total, score, attempt) VALUES ('${
+        auth.student_id
+      }', '${req.body.test_id}', '${auth.teacher_id}', '${
+        auth.group_id
+      }', ${correct}, ${total}, ${score}, ${attempts.rowCount + 1}) RETURNING result_id`,
+      (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500);
+        } else {
+          let queryString = `INSERT INTO resultdata (result_id, word, answer, correct, line_number) VALUES `;
+          let line_number = 1;
+          testData.forEach((x) => {
+            queryString = queryString.concat(
+              `('${data.rows[0].result_id}', '${x.word.replace("'", "''")}', '${x.ans.replace(
+                "'",
+                "''"
+              )}', ${x.word === x.ans ? true : false}, ${line_number}),`
+            );
+            line_number++;
+          });
+          queryString = queryString.slice(0, -1);
+          db.query(queryString, (err, sdata) => {
+            if (err) {
+              console.error(err);
+              db.query(`DELETE FROM results WHERE result_id = '${data.rows[0].result_id}'`);
+              return res.status(500);
+            } else {
+              res.status(200).json();
+            }
+          });
+        }
       }
-    })
+    );
   });
 });
 
--- routes/v2/teacher.js
+++ routes/v2/teacher.js
@@ -17,28 +17,29 @@ router.post('/tests/create', (req, res) => {
   let testName = req.body.testName;
   let attempts = req.body.attempts;
   let words = req.body.words.split(',');
-  console.log(req.files);
-
   db.query(
     `INSERT INTO tests (teacher_id, test_name, attempts) VALUES ('${auth.teacher_id}', '${testName}', ${attempts}) RETURNING test_id`,
     (err, data) => {
       if (err) {
+        console.error(err);
         return res.status(500);
       } else {
         let queryString = `INSERT INTO testlines (test_id, line_number, word, audio_path) VALUES `;
         for (i = 0; i < words.length; i++) {
           queryString = queryString.concat(
-            `('${data.rows[0].test_id}', ${i + 1}, '${
-              words[i]
-            }', 'https://spelling-tests.s3-us-west-2.amazonaws.com/${auth.teacher_id}/${words[i]
-              .replace("'", '%27')
-              .toLowerCase()}.wav'),`
+            `('${data.rows[0].test_id}', ${i + 1}, '${words[i].replace(
+              "'",
+              "''"
+            )}', 'https://spelling-tests.s3-us-west-2.amazonaws.com/${auth.teacher_id}/${words[
+              i
+            ].replace("'", "''")}.wav'),`
           );
         }
         queryString = queryString.slice(0, -1);
         db.query(queryString, (err, sdata) => {
           if (err) {
             res.status(500);
+            console.error(err);
             db.query(`DELETE FROM tests WHERE test_id='${data.rows[0].test_id}'`);
           } else {
             if (req.files.file[0] === undefined) {
@@ -74,35 +75,31 @@ router.post('/tests/create', (req, res) => {
                 );
               });
             } else {
+              // multiple files
               req.files.file.map((file) => {
-                file.mv(path.join(audioPath, `/${file.name.replace("'", '%27')}`), () => {
-                  fs.readFile(
-                    path.join(audioPath, `/${file.name.replace("'", '%27')}`),
-                    (err, fsdata) => {
-                      if (err) {
-                        console.error(err);
+                file.mv(path.join(audioPath, `/${file.name}`), () => {
+                  fs.readFile(path.join(audioPath, `/${file.name}`), (err, fsdata) => {
+                    if (err) {
+                      console.error(err);
+                      db.query(`DELETE FROM tests WHERE test_id='${data.rows[0].test_id}'`);
+                      return res.status(500);
+                    }
+                    const params = {
+                      Bucket: 'spelling-tests',
+                      Key: `${auth.teacher_id}/${file.name.toLowerCase()}`,
+                      Body: fs.createReadStream(path.join(audioPath, `/${file.name}`)),
+                      ACL: 'public-read',
+                    };
+                    s3.upload(params, (s3err, s3data) => {
+                      if (s3err) {
+                        console.error(s3err);
                         db.query(`DELETE FROM tests WHERE test_id='${data.rows[0].test_id}'`);
                         return res.status(500);
+                      } else {
+                        res.status(201).json();
                       }
-                      const params = {
-                        Bucket: 'spelling-tests',
-                        Key: `${auth.teacher_id}/${file.name.replace("'", '%27').toLowerCase()}`,
-                        Body: fs.createReadStream(
-                          path.join(audioPath, `/${file.name.replace("'", '%27')}`)
-                        ),
-                        ACL: 'public-read',
-                      };
-                      s3.upload(params, (s3err, s3data) => {
-                        if (s3err) {
-                          console.error(s3err);
-                          db.query(`DELETE FROM tests WHERE test_id='${data.rows[0].test_id}'`);
-                          return res.status(500);
-                        } else {
-                          res.status(201).json();
-                        }
-                      });
-                    }
-                  );
+                    });
+                  });
                 });
               });
             }
