d3c478f7da4be76488422e383ab53d615d45e06e
spencer119 <spencernhamilton@gmail.com>
2021-02-10 18:13:44 -0700

    v2.2

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.static('./data/'));
 app.use(express.json());
 app.use(fileUpload());
-const maintenance = false;
 app.options('*', cors());
 
 const checkAuth = async (req, res, next) => {
