42c80d3cd5f2249aa5a8a48c3fa4ca4326ab0e90
spencer119 <spencernhamilton@gmail.com>
2021-03-03 08:41:05 -0700

    working on exports

--- routes/v2/teacher.js
+++ routes/v2/teacher.js
@@ -200,5 +200,71 @@ router.get('/export', async (req, res) => {
         .sendFile(path.join(__dirname, '../../data/export.csv'));
     });
 });
+router.get('/report', async (req, res) => {
+  let auth = res.locals.auth;
+  let exportData = await db.query(
+    `SELECT student_id, test_id, group_id, score, total, correct, created_at, attempt FROM results WHERE teacher_id = '${auth.teacher_id}' LIMIT 1844674407370955161`
+  );
+  let studentData = await db.query(
+    `SELECT student_id, first_name, last_name FROM students WHERE teacher_id = '${auth.teacher_id}'`
+  );
+  let groupData = await db.query(
+    `SELECT group_id, group_name FROM groups WHERE teacher_id = '${auth.teacher_id}'`
+  );
+  let testData = await db.query(
+    `SELECT test_id, test_name FROM tests WHERE teacher_id = '${auth.teacher_id}'`
+  );
+  let replacedValues = exportData.rows.map((row) => {
+    // Change date to a more readable format
+    let newDate = new Date(row.created_at);
+    row['Date'] = newDate.toLocaleDateString();
+    delete row.created_at;
+
+    // Replace student_id with the first and last name
+    let student = studentData.rows.find(
+      (student) => student.student_id === row.student_id
+    );
+    row['First Name'] = student.first_name;
+    row['Last Name'] = student.last_name;
+    delete row.student_id;
+
+    // Replace test_id with test name
+    let _test = testData.rows.find((tst) => tst.test_id === row.test_id);
+    row['Test Name'] = _test.test_name;
+    delete row.test_id;
+
+    // Replace group_id with group name
+    let grp = groupData.rows.find((grp) => grp.group_id === row.group_id);
+    row['Group Name'] = grp.group_name;
+    delete row.group_id;
+
+    row['Score'] = row.score;
+    delete row.score;
+
+    row['Correct'] = row.correct;
+    delete row.correct;
+
+    row['Total'] = row.total;
+    delete row.total;
+
+    row['Attempt'] = row.attempt;
+    delete row.attempt;
+
+    return row;
+  });
+  const ws = fs.createWriteStream('./data/export.csv');
+  const jsonData = JSON.parse(JSON.stringify(exportData.rows));
+  fastcsv
+    // write the JSON data as a CSV file
+    .write(jsonData, { headers: true })
+    .pipe(ws)
+
+    .on('finish', () => {
+      return res
+        .status(200)
+        .attachment('exports.csv')
+        .sendFile(path.join(__dirname, '../../data/export.csv'));
+    });
+});
 
 module.exports = router;
